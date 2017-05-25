e0199f6182704100c756068fb01e995951dbd5b5
Ivy Patton <lilaivy@gmail.com>
2017-05-25 10:20:11 -0700

    added guard clause  into Single to make sure we had data to render

--- image-gallery/src/App.js
+++ image-gallery/src/App.js
@@ -18,7 +18,9 @@ export default class App extends Component {
 
   componentDidMount() {
     imageData.get()
-      .then(images => this.setState({ images }));
+      .then(images => {
+        this.setState({ images })
+      });
   }
 
   // slectImage(selectedImage) {
@@ -29,27 +31,27 @@ export default class App extends Component {
   render() {
     const { images } = this.state;
     return (
-      <div >
+    <div >
 
-        <div>
-          <h2>List Gallery</h2>
-          <List
-            images={images}
-          />
-        </div>
-        <div>
-          <h2>Thumbnail Gallery</h2>
-          <Thumbnail
-            images={images}
-          />
-        </div>
-        <div>
-          <h2>Single Image Gallery</h2>
-          <Single
-            images={images}
-          />
-        </div>
+      <div>
+        <h2>List Gallery</h2>
+        <List
+          images={images}
+        />
       </div>
+      <div>
+        <h2>Thumbnail Gallery</h2>
+        <Thumbnail
+          images={images}
+        />
+      </div>
+      <div>
+        <h2>Single Image Gallery</h2>
+        <Single
+          images={images}
+        />
+      </div>
+    </div>
     );
   }
 }
--- image-gallery/src/galleries/Single.js
+++ image-gallery/src/galleries/Single.js
@@ -1,50 +1,56 @@
 import React, { Component } from 'react';
 
 export default class Single extends Component {
-    // constructor(props) {
-    //     super(props);
+    constructor(props) {
+        super(props);
 
-        // this.state ={
-        //     currentImage:
-        //     nextImage:
-        //     previousImage:
+        this.state = {
+            currentImage:
+            nextImage:
+            previousImage:
 
-        // }
-
-        //Q: how do I get the property of image.url off of images array?
-        //TODO: add on click to go to next/previous bunny
-
-        nextBunny(nextBunny) {
-            this.nextBunny({ nextBunny });
-        }
-
-         previousBunny(previousBunny) {
-            this.previousBunny({ previousBunny });
-        }
-
-
-
-        render() {
-            return (
-                <div style={{
-                    display: 'inline-block'
-                }} >
-                    <span className='nextBunny' style={{
-                        fontSize: '50px'
-
-                    }} >
-                        &larr;</span>
-
-                    <span>
-                        <img src='http://static.boredpanda.com/blog/wp-content/uploads/2015/09/cute-bunnies-25__605.jpg'/>
-                    </span>
-
-                    <span className='previousBunny' style={{
-                        fontSize: '50px'
-                    }} >
-                        &rarr;</span>
-                </div>
-            )
         }
     }
-// }
+
+    //Q: how do I get the property of image.url off of images array?
+    //TODO: add on click to go to next/previous bunny
+
+    nextBunny(nextBunny) {
+        this.nextBunny({ nextBunny });
+    }
+
+    previousBunny(previousBunny) {
+        this.previousBunny({ previousBunny });
+    }
+
+
+
+    render() {
+        if (!this.props.images.length) {
+            return null;
+        }
+        return (
+            <div style={{
+                display: 'inline-block'
+            }} >
+                <span className='nextBunny' style={{
+                    fontSize: '50px'
+
+                }} >
+                    &larr;</span>
+
+                <span>
+                    <img src={this.props.images[1].url} />
+                    <p>{this.props.images[1].title}</p>
+                    <p>{this.props.images[1].description} </p>
+                </span>
+
+                <span className='previousBunny' style={{
+                    fontSize: '50px'
+                }} >
+                    &rarr;</span>
+            </div>
+        )
+    }
+}
+
