d56ab2fb22ae2cab76d323c88d4da0062624bc6a
Ivy Patton <lilaivy@gmail.com>
2017-06-01 10:25:37 -0700

    stringified match, location, history on home/intro

--- image-gallery/src/components/IntroView.js
+++ image-gallery/src/components/IntroView.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import introImage from '../photos/introImage.jpg'
 
-export default function Intro() {
+export default function Intro( {match, location, history}) {
 
     return (
         <div className='intro-image brighten'>
+            <pre>{JSON.stringify(match, true, 2)}</pre>
+            <pre>{JSON.stringify(location, true, 2)}</pre>
+            <pre>{JSON.stringify(history, true, 2)}</pre>
+            
             <a className="video" href='https://www.youtube.com/watch?v=cKC-XBwQ_pA'>
                 <img src={introImage} alt='standing on boards' />
             </a>
