684925b87ea3ddaac4a6177465c5a07c9ec64101
Ivy Patton <lilaivy@gmail.com>
2017-06-02 12:06:16 -0700

    all views working on clicked image

--- image-gallery/src/components/ViewButtons.js
+++ image-gallery/src/components/ViewButtons.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import List from './List';
-import Thumbnail from './Thumbnail';
-import Gallery from './Gallery'
 
-const Views={List, Thumbnail, Gallery}
 
 
 export default function Buttons({match}){
+
 
     return(
         <div>
