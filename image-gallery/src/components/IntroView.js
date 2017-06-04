ad0a28f63f9aaf5937d740c779c0bf173b740b50
Ivy Patton <lilaivy@gmail.com>
2017-06-04 01:38:32 -0700

    home page includes link to albums

--- image-gallery/src/components/IntroView.js
+++ image-gallery/src/components/IntroView.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import introImage from '../photos/introImage.jpg'
+import introImage from '../photos/introImage.jpg';
+import { Link } from 'react-router-dom'
 
 
-export default function Intro( {match, location, history}) {
+export default function Intro({ match, location, history }) {
 
     return (
         <div className='intro-image brighten'>
 
-            <a className="video" href='https://www.youtube.com/watch?v=cKC-XBwQ_pA'>
-                <img src={introImage} alt='standing on boards' />
-            </a>
-            <span className="play-video"><h1>PLAY VIDEO</h1></span>
+            <Link className="Link" to={`/albums`}>
+                <a className="video">
+                    <img src={introImage} alt='standing on boards' />
+                </a>
+            <span className="play-video" ><h1>GO TO IMAGE GALLERY</h1></span>
+            </Link>
 
         </div>
     )
