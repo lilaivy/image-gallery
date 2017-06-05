ca4e1ec7a0b048af3dfd534b5c6300b1e46eafbe
Ivy Patton <lilaivy@gmail.com>
2017-06-04 18:02:28 -0700

    app tests passing

--- image-gallery/src/_tests_/App.test.js
+++ image-gallery/src/_tests_/App.test.js
@@ -1,7 +1,10 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
-import renderer from 'react-test-renderer'
+import renderer from 'react-test-renderer';
+
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
