9d1650f59461cdf7477e97eb11742b642da3a4de
Ivy Patton <lilaivy@gmail.com>
2017-06-05 13:00:00 -0700

    changes componentDidMount to fetch in albums.js

--- image-gallery/app/src/components/Albums.js
+++ image-gallery/app/src/components/Albums.js
@@ -12,31 +12,37 @@ export default class Albums extends Component {
         }
     }
 
-    // componentDidMount() {
-    //     return (copyAlbumsApi.getAlbums()
-    //         .then((albums) => {
-    //             this.setState({ albums })
-    //         }))
-    // }
+    componentDidMount() {
+        const { params } = this.props.match;
+        this.fetchAlbum(params.albumId)
+    }
+
+
+    fetchAlbum(albumId) {
+        fetch(`/api/albums/${albumId}`)
+            .then(res => res.json())
+            .then(album => this.setState({ album }))
+            .catch(error => console.log(error));
+    }
 
     render() {
         const { match } = this.props;
         const { albums } = this.state;
-     if(!albums) return <div>Loading ...</div>;
+        if (!albums) return <div>Loading ...</div>;
 
         return (
             <div>
                 <ul>
-                    {albums.map(album => <Album 
+                    {albums.map(album => <Album
                         key={album._id} {...album}
                         url={`${match.url}/${album._id}`} />
                     )}
                 </ul>
 
-                <Route path={`${match.url}/:albumId`} render={({match}) => {
-                   const album= albums.find(album => album._id === match.params.albumId)
-                   return <AlbumContent {...album} match={match}/>
-                } }/>
+                <Route path={`${match.url}/:albumId`} render={({ match }) => {
+                    const album = albums.find(album => album._id === match.params.albumId)
+                    return <AlbumContent {...album} match={match} />
+                }} />
             </div>
 
         );
--- image-gallery/server/lib/routes/albums.js
+++ image-gallery/server/lib/routes/albums.js
@@ -15,19 +15,30 @@ router
     .get('/:id', (req, res, next) => {
         const id = req.params.id;
         Album.findById(id).lean()
-        .then(album => {
-            if(!album)res.status(404).send(`${id} not found`);
-            else res.send(album);
-        })
-        .catch(next);
+            .then(album => {
+                if (!album) res.status(404).send(`${id} not found`);
+                else res.send(album);
+            })
+            .catch(next);
     })
-
     .post('/', (req, res, next) => {
-        new Album(req.body)
-            .save()
+        const album = req.body;
+        Album.find({ name: album.name })
+            .count()
+            .then(count => {
+                if(count > 0) throw { code: 400, error: 'album name must be unique' };
+                return new Album(album).save();
+            })
             .then(album => res.send(album))
             .catch(next);
     })
+
+    // .post('/', (req, res, next) => {
+    //     new Album(req.body)
+    //         .save()
+    //         .then(album => res.send(album))
+    //         .catch(next);
+    // })
 
     .put('/:id', (req, res, next) => {
         Album.findByIdAndUpdate(req.params.id, req.body, { new: true })
@@ -35,17 +46,13 @@ router
             .catch(next);
     })
 
-     .delete('/:id', (req, res, next) => {
+    .delete('/:id', (req, res, next) => {
         Album.findByIdAndRemove(req.params.id)
-            .then(response => {
-                res.send({ removed: response ? true : false });
+            .then(album => {
+                res.send({ removed: !! album });
             })
             .catch(next);
 
-
-
-    
-
-
+    });
 
 module.exports = router;
